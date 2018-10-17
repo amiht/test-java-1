9ad4ec3bdf1490a038e8ffa7a435325917a2fc3d
amiht <amiht.ranjan@gmail.com>
2018-10-18 01:08:09 +0530

    add great works

--- src/com/test/demotest/DemoTest.java
+++ src/com/test/demotest/DemoTest.java
@@ -5,6 +5,7 @@ public class DemoTest {
 	public static void main(String[] args) {
 		System.out.println("First GitHub project!");
 		System.out.println("DONE!!");
+		System.out.println("Great Works!!");
 	}
 
 }
